bf7e9991b722c945c3dd207f131e962c78823ace
Bobo1111111 <boyuanspace@gmail.com>
2022-12-12 11:30:53 +0800

    add slider class

--- setting_menu.py
+++ setting_menu.py
@@ -3,6 +3,7 @@ from gameinit import *
 from constant import *
 from button import *
 from constant import *
+from slider import *
 
 pic_setting = pygame.image.load("setting.jpg")
 pic_setting_new = pygame.transform.scale(pic_setting,(SCREEN_WIDTH,SCREEN_HEIGHT))
@@ -22,17 +23,14 @@ def setting_menu(screen, call_state):
     music_rect = music_word.get_rect(center=(SCREEN_WIDTH/7,SCREEN_HEIGHT/2.5))
     screen.blit(music_word, music_rect)
 
+    sound_font = pygame.font.SysFont(font__, 30)
+    sound_word = sound_font.render("Sound", True, WHITE)
+    sound_rect = sound_word.get_rect(center=(SCREEN_WIDTH/7,SCREEN_HEIGHT/2))
+    screen.blit(sound_word, sound_rect)
+
     back_button = button(SCREEN_WIDTH/2,SCREEN_HEIGHT/7*6,SCREEN_WIDTH/3,SCREEN_HEIGHT/8,text="Back", norm_color=WHITE, on_color=GREY)
-    
-    norm_color = WHITE
-    on_color = GREEN
-    left_limit = SCREEN_WIDTH/7*2
-    right_limit = SCREEN_WIDTH/7*5
-    r = 10
-    y = SCREEN_HEIGHT/2.5
-    position = (left_limit,y)
-    clicked = False
-    over = False
+    music_slider = slider(SCREEN_WIDTH/7*2,SCREEN_WIDTH/7*5,SCREEN_HEIGHT/2.5,False)
+    sound_slider = slider(SCREEN_WIDTH/7*2,SCREEN_WIDTH/7*5,SCREEN_HEIGHT/2,False)
 
     while True:
         for event in pygame.event.get():
@@ -49,29 +47,9 @@ def setting_menu(screen, call_state):
         back_button.draw(screen,mouse_pos)
         screen.blit(title, text_rect)
         screen.blit(music_word, music_rect)
-
-        #is_over
-        if((position[0]-mouse_pos[0])**2+(position[1]-mouse_pos[1])**2) < r**2:
-            over = True
-        else:
-            over = False
-
-        if pygame.mouse.get_pressed()[0] and over:
-            clicked = True
-        if not pygame.mouse.get_pressed()[0]:
-            clicked = False
-
-        if clicked:
-            if left_limit < mouse_pos[0] < right_limit:
-                position = (mouse_pos[0],y)
-            elif(left_limit > mouse_pos[0]):
-                position = (left_limit,y)
-            elif(mouse_pos[0] > right_limit):
-                position = (right_limit,y)
-            music_slide = pygame.draw.circle(screen,on_color, position, r, 0)
-
-        if not clicked:
-            music_slide = pygame.draw.circle(screen,norm_color, position, r, 0)
+        screen.blit(sound_word, sound_rect)
+        music_slider.draw(screen,mouse_pos,pygame.mouse.get_pressed()[0])
+        sound_slider.draw(screen,mouse_pos,pygame.mouse.get_pressed()[0])
         pygame.display.flip()
         fpsClock.tick(FPS)
 
---
+++ slider.py
@@ -0,0 +1,51 @@
+import pygame
+from constant import *
+
+class slider():
+    def __init__(self,low_limit,high_limit,y,press,radius = 10, initial = 0,
+     activate = False, norm_color= WHITE, on_color= GREEN,linecolor = BLACK):
+        self.low_limit = low_limit
+        self.high_limit = high_limit
+        self.y = y
+        self.r = radius
+        self.initial = low_limit
+        self.press = press
+        self.activate = activate
+        self.norm_color = norm_color
+        self.on_color = on_color
+        self.position = (self.initial,self.y)
+        self.line_color = linecolor
+
+    def draw(self,screen,mouse_pos,pres):
+        over = self.Over(mouse_pos)
+        if pres and over:
+            self.activate = True
+        if not pres:
+            self.activate = False
+        pygame.draw.line(screen, self.line_color,(self.low_limit,self.y),(self.high_limit,self.y),width = 5)
+        if self.activate:
+            self.newpos(mouse_pos[0])
+            pygame.draw.circle(screen,self.on_color, self.position, self.r, 0)
+        else:
+            pygame.draw.circle(screen,self.norm_color, self.position, self.r, 0)
+
+    def Over(self, m_pos):
+        if((self.position[0]-m_pos[0])**2+(self.position[1]-m_pos[1])**2) < self.r**2:
+            return True
+        return False
+
+    # def clicked(self,activate,m_pos):
+    #     if self.press and self.Over(m_pos):
+    #         return True
+    #     elif not self.press:
+    #         return False
+    #     else:
+    #         return activate
+
+    def newpos(self,mouse_x):
+        if self.low_limit < mouse_x < self.high_limit:
+            self.position =  (mouse_x,self.y)
+        elif(self.low_limit >= mouse_x):
+            self.position = (self.low_limit , self.y)
+        elif(mouse_x >= self.high_limit):
+            self.position = (self.high_limit, self.y)
