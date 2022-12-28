be3fa62097412b19a9b9fc7e4acd1dae64e3271c
casey1023 <caseyfj15@gmail.com>
2022-12-28 16:55:08 +0800

    more comments

--- pause_menu.py
+++ pause_menu.py
@@ -4,46 +4,67 @@ from gameinit import *
 #### DNF
 
 def pause_menu(screen, call_state):
-    constant=readconstant()
+    #const init
+    constant = readconstant()
     locals().update(constant)
     
     fpsClock = pygame.time.Clock()
 
+    #screen  init
     screen.fill(BLACK)
     font = pygame.font.SysFont(font__, 50)
     title = font.render("Pause Menu", True, WHITE)
     text_rect = title.get_rect(center=(SCREEN_WIDTH/2, SCREEN_HEIGHT/6))
     screen.blit(title, text_rect)
 
-    back_button=button(SCREEN_WIDTH/2,SCREEN_HEIGHT/7*2.5,SCREEN_WIDTH/3,SCREEN_HEIGHT/8,text="Continue")
-    setting_button=button(SCREEN_WIDTH/2,SCREEN_HEIGHT/7*3.5,SCREEN_WIDTH/3,SCREEN_HEIGHT/8,text="Setting")
-    main_button=button(SCREEN_WIDTH/2,SCREEN_HEIGHT/7*4.5,SCREEN_WIDTH/3,SCREEN_HEIGHT/8,text="Main Menu")
-    quit_button=button(SCREEN_WIDTH/2,SCREEN_HEIGHT/7*5.5,SCREEN_WIDTH/3,SCREEN_HEIGHT/8,text="Quit")
+    #button init
+    back_button =       button(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 7 * 2.5, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 8, text = "Continue")
+    setting_button =    button(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 7 * 3.5, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 8, text = "Setting")
+    main_button =       button(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 7 * 4.5, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 8, text = "Main Menu")
+    quit_button =       button(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 7 * 5.5, SCREEN_WIDTH / 3, SCREEN_HEIGHT / 8, text = "Quit")
 
     
     while True:
         for event in pygame.event.get():
+            #back to play
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     return {'from': 'pause_menu', 'to': 'play'}
+            
+            #quit game
             elif event.type == pygame.QUIT:
                 return {'from': "pause_menu", 'to': 'quit'}
+            
+
             elif event.type == pygame.MOUSEBUTTONUP:
                 pos = pygame.mouse.get_pos()
+
+                #play
                 if back_button.isOver(pos):
                     return {'from': 'pause_menu', 'to': 'play'}
+
+                #setting_menu
                 elif setting_button.isOver(pos):
                     return {'from': 'pause_menu', 'to': 'setting_menu'}
+                
+                #main_menu
                 elif main_button.isOver(pos):
                     return {'from': 'pause_menu', 'to': 'main_menu'}
+                
+                #quit game
                 elif quit_button.isOver(pos):
                     return {'from': "pause_menu", 'to': 'quit'}
         
+        #update mouse pos
         mouse_pos = pygame.mouse.get_pos()
+
+        #draw button
         back_button.draw(screen,mouse_pos)
         setting_button.draw(screen,mouse_pos)
         main_button.draw(screen,mouse_pos)
         quit_button.draw(screen,mouse_pos)
+
+        #update screen
         pygame.display.flip()
         fpsClock.tick(FPS)
 
--- slider.py
+++ slider.py
@@ -1,35 +1,39 @@
 import pygame
 from setting import *
 
-constant=readconstant()
+constant = readconstant()
 locals().update(constant)
 
 class slider():
     working = False
 
-    def __init__(self,low_limit,high_limit,y,radius = 15, initial = 0,
-     norm_color= WHITE, on_color= GREEN,linecolor = BLACK):
+    def __init__(self, low_limit, high_limit, y, radius = 15, initial = 0, \
+     norm_color = WHITE, on_color = GREEN, linecolor = BLACK):
         self.low_limit = low_limit
         self.high_limit = high_limit
         self.y = y
         self.r = radius
-        self.initial = low_limit + (high_limit - low_limit)*initial
+        self.initial = low_limit + (high_limit - low_limit) * initial
         self.activate = False
         self.norm_color = norm_color
         self.on_color = on_color
-        self.position = (self.initial,self.y)
+        self.position = (self.initial, self.y)
         self.line_color = linecolor
 
-    def draw(self,screen,mouse_pos,pres):
+    def draw(self, screen, mouse_pos, pres):
         over = self.Over(mouse_pos)
         online = self.on_line(mouse_pos)
-        if pres and ( over or online )and not slider.working:
+
+        if pres and (over or online) and not slider.working:
             self.activate = True
             slider.working = True
+        
         if not pres:
             self.activate = False
             slider.working = False
-        pygame.draw.line(screen, self.line_color,(self.low_limit,self.y),(self.high_limit,self.y),width = 5)
+        
+        pygame.draw.line(screen, self.line_color, (self.low_limit, self.y), (self.high_limit, self.y), width = 5)
+
         if self.activate:
             self.newpos(mouse_pos[0])
             pygame.draw.circle(screen,self.on_color, self.position, self.r, 0)
@@ -37,12 +41,12 @@ class slider():
             pygame.draw.circle(screen,self.norm_color, self.position, self.r, 0)
 
     def Over(self, m_pos):
-        if((self.position[0]-m_pos[0])**2+(self.position[1]-m_pos[1])**2) < self.r**2:
+        if((self.position[0] - m_pos[0]) ** 2 + (self.position[1] - m_pos[1]) ** 2) < self.r ** 2:
             return True
         return False
     
     def on_line(self, m_pos):
-        if(self.low_limit < m_pos[0] < self.high_limit and self.y - 1/2*self.r < m_pos[1] < self.y + 1/2*self.r):
+        if(self.low_limit < m_pos[0] < self.high_limit and self.y - 1 / 2 * self.r < m_pos[1] < self.y + 1 / 2 * self.r):
             return True
         return False
 
@@ -56,11 +60,13 @@ class slider():
 
     def newpos(self,mouse_x):
         if self.low_limit < mouse_x < self.high_limit:
-            self.position =  (mouse_x,self.y)
+            self.position =  (mouse_x, self.y)
+
         elif(self.low_limit >= mouse_x):
-            self.position = (self.low_limit , self.y)
+            self.position = (self.low_limit, self.y)
+
         elif(mouse_x >= self.high_limit):
             self.position = (self.high_limit, self.y)
     
     def value(self):
-        return (self.position[0]-self.low_limit)/(self.high_limit-self.low_limit)
+        return (self.position[0] - self.low_limit) / (self.high_limit - self.low_limit)
