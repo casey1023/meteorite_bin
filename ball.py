7ecc6e6f573f74215ec3b90014fb4d46662b3499
casey1023 <caseyfj15@gmail.com>
2022-12-28 17:19:45 +0800

    more rearrangements and comments

--- ball.py
+++ ball.py
@@ -3,47 +3,59 @@ from pygame.math import Vector2
 from time import time
 from setting import *
 
-constant=readconstant()
+#get const
+constant = readconstant()
 locals().update(constant)
 
 class ball():
-    def __init__(self,screen, position,init_v=0, radius=5):
+    def __init__(self, screen, position, init_v = 0, radius = 5):
         self.position = Vector2(position)
         self.radius = radius
         if init_v:
-            self.v=Vector2(init_v)
+            self.v = Vector2(init_v)
         else:
-            self.v = Vector2(0,0)
-        self.screen=screen
-        self.t=time()
-        self.invincible=True
+            self.v = Vector2(0, 0)
+        self.screen = screen
+        self.t = time()
+        self.invincible = True
+
     def __str__(self):
-        return "Position : "+str(self.position)+"\nV : "+str(self.v)
-    def move(self,mp):
-        p=self.position
+        return "Position : " + str(self.position) + "\nV : " + str(self.v)
+    
+    def move(self, mp):
+        p = self.position
         # d=1/ (mp-p).length()**2 * (mp-p).normalize()*2000
-        d=(mp-self.position)*0.0015
-        self.v+=d
+        d = (mp - self.position) * 0.0015
+        self.v += d
+
         #set speed limit
-        speedlimit=12
-        if self.v.length()>speedlimit:
-            self.v=self.v.normalize()*speedlimit
+        speedlimit = 12
+        if self.v.length() > speedlimit:
+            self.v = self.v.normalize() * speedlimit
         self.position += self.v
+
         #check if hit boarder
-        if self.position.x<0 or self.position.x>SCREEN_WIDTH:
-            self.v.x=-self.v.x
-            self.v=self.v*0.95
-        if self.position.y<0 or self.position.y>SCREEN_HEIGHT:
-            self.v.y=-self.v.y
-            self.v=self.v*0.95
+        if self.position.x < 0 or self.position.x > SCREEN_WIDTH:
+            self.v.x = -self.v.x
+            self.v = self.v * 0.95
+
+        if self.position.y < 0 or self.position.y > SCREEN_HEIGHT:
+            self.v.y = -self.v.y
+            self.v = self.v * 0.95
+
         if self.invincible:
-            pygame.draw.circle(self.screen,LIGHTRED,self.position,self.radius,0)
+            pygame.draw.circle(self.screen, LIGHTRED, self.position, self.radius, 0)
+
         else:
-            pygame.draw.circle(self.screen,RED,self.position,self.radius,0)
-    def iscolide(self,pos2,r2):
-        return Vector2(pos2-self.position).length()<=r2+self.radius
+            pygame.draw.circle(self.screen, RED, self.position, self.radius, 0)
+        
+        return None
+        
+    def iscolide(self, pos2, r2):
+        return Vector2(pos2 - self.position).length() <= r2 + self.radius
+
     def isinvincible(self):
-        if self.invincible and time()-self.t>1.5:
+        if self.invincible and time() - self.t > 1.5:
             # print("set to not invincible")
-            self.invincible=False
+            self.invincible = False
         return self.invincible
